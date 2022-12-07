755ea4b89e7d78bba58d0e19a62b65a3b86366db
Andrius Bogda <AndriusBogda@coherentsolutions.com>
2022-12-07 13:31:23 +0200

    kazkas veikia pagaliau

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="end">
+<head>
+    <meta charset="UF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scaleable=no, initial-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+<body>
+<div>
+    @yield('content')
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ConferenceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/forum', function () {
-    return view('forum');
-});
+Route::resource('conference', ConferenceController::class)->only(['index']);
